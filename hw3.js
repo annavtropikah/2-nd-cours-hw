39e873405a746fcfad2b1744043594a15c2c3eb9
«Annavtropikah» <kozhevnikova.it@gmail.com>
2023-10-18 14:05:27 +0300

    ДЗ 3

---
+++ hw3.js
@@ -0,0 +1,95 @@
+// //Задание 1
+// let password = 'пароль';
+// let passwordUser = prompt('Введите пароль');
+// if (passwordUser===password) {
+//     alert ('Пароль введен верно');
+// } else {
+//     alert ('Пароль введен неправильно');
+// }
+
+// //Задание 2
+// let c = 2;
+// if (c>0 && c<10) {
+//     console.log ('Верно');
+// } else {
+// console.log ('Неверно');
+// }
+
+// //Задание 3
+// let d = 102;
+// let e = 9;
+// if (d > 100 || e > 100) {
+//     console.log ('Верно');
+// } else {
+//     console.log ('Неверно');
+// }
+
+
+// //Задание 4
+// let a = '2';
+// let b = '3';
+// console.log (Number(a)+ Number(b));
+
+
+// //Задание 5
+// let monthNumber = 4;
+// switch (monthNumber) {
+//         case 1:
+//             case 2:
+//             case 12:
+//             console.log ('Зима');
+//         break;
+//         case 3:
+//             case 4:
+//             case 5:
+//                 console.log ('Весна');
+//         break;
+//         case 6:
+//             case 7:
+//             case 8:
+//                 console.log ('Лето');
+//         break;
+//         case 9:
+//             case 10:
+//             case 11:
+//                 console.log ('Осень');
+//         break;
+//     default:
+//         console.log ('Неизвестный месяц');
+//         break;
+// }
+
+
+//Задание 7
+
+// let userInp=prompt(' Введите любое число');
+// if (!isNaN(userInp)){
+
+//     if (userInp%2===0) {
+//   console.log('четное');  
+// }
+// else {
+//     console.log('нечетное');
+// }
+// }
+// else {
+//     console.log("Не число");
+// }
+
+//Задание 8
+// let clientOS=1;
+// let prodYear= 2015;
+// if (clientOS===0 && prodYear>=2015) {
+//     console.log('Установите версию приложения для iOS по ссылке');
+// };
+// if (clientOS===0 && prodYear<2015)  {
+//     console.log('Установите ОБЛЕГЧЕННУЮ версию приложения для iOS по ссылке');
+// };
+// if (clientOS===1 && prodYear>=2015) {
+//     console.log('Установите версию приложения для Android по ссылке');
+// };
+// if (clientOS===1 && prodYear<2015) {
+//     console.log('Установите  ЛЕГКУЮ версию приложения для Android по ссылке');
+// };
+
+/* ??????Важно: вложенность больше двух этажей не допускается (условный оператор внутри условного оператора).*/

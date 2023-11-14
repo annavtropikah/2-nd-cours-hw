c194776cc5e5a63b159060740d5edba0cd830710
«Annavtropikah» <kozhevnikova.it@gmail.com>
2023-11-14 17:12:55 +0300

    доработка игры2

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ function rememberWords() {
     let firstInp = prompt('Чему равнялся первый элемент массива?');
     let lastInp = prompt('Чему равнялся последний элемент массива?');
 
-    if (firstInp == array[0].toLowerCase() && lastInp == array[arr.length - 1].toLowerCase()) {
+    if (firstInp == array[0].toLowerCase() && lastInp == array[6].toLowerCase()) {
         alert('Поздравляем! Это успех!');
-    } else if (firstInp.toLowerCase() == array[0].toLowerCase() || lastInp.toLowerCase() == array[arr.length - 1].toLowerCase()) {
+    } else if (firstInp.toLowerCase() == array[0].toLowerCase() || lastInp.toLowerCase() == array[6].toLowerCase()) {
         alert('Вы были близки к победе!');
     } else {
         alert('Вы ответили неверно,но в следующий раз все получится! ');
